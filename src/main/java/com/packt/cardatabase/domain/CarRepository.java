7fdc1a3a8faf29e71e37c98edba206a1ec4b82cc
XC <xc@abc.com>
2024-01-22 10:53:39 +0800

    update - Mon, Jan 22, 2024 10:53:39

--- src/main/java/com/packt/cardatabase/domain/CarRepository.java
+++ src/main/java/com/packt/cardatabase/domain/CarRepository.java
@@ -1,15 +1,16 @@
 package com.packt.cardatabase.domain;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import java.util.List;
 
 
 public interface CarRepository extends CrudRepository<Car, Long> {
     // Fetch cars by brand
-    List<Car> findByBrand(String brand);
+    // List<Car> findByBrand(String brand);
 
     // Fetch cars by color
-    List<Car> findByColoCars(String color);
+    List<Car> findByColor(String color);
 
     // Fetch cars by model year
     List<Car> findByModelYear(int year);
@@ -22,4 +23,12 @@ public interface CarRepository extends CrudRepository<Car, Long> {
 
     // Fetch cars by brand and sort by year
     List<Car> findByBrandOrderByModelYearAsc(String brand);
+
+    // Fetch cars by brand using SQL
+    @Query("select c from Car c where c.brand = ?1")
+    List<Car> findByBrand(String brand);
+
+    // Fetch cars by brand using SQL
+    @Query("select c from Car c where c.brand like %?1")
+    List<Car> findByBrandEndsWith(String brand);
 }
