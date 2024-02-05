c4dd694ce7ad6ebaa2bd79fa4c47558dc5a2d053
XC <xc@abc.com>
2024-02-05 17:18:35 +0800

    mod - Mon, Feb  5, 2024 17:18:36

--- src/test/java/com/packt/cardatabase/CardatabaseApplicationTests.java
+++ src/test/java/com/packt/cardatabase/CardatabaseApplicationTests.java
@@ -2,6 +2,7 @@ package com.packt.cardatabase;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -21,6 +22,7 @@ class CardatabaseApplicationTests {
 	private CarController carController;
 
 	@Test
+	@DisplayName("First example test case")
 	void contextLoads() {
 		assertThat(carController).isNotNull();
 	}
