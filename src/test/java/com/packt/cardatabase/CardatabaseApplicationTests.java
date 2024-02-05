99576f3b04c28778a3eddd1c5e6ec4385024813d
XC <xc@abc.com>
2024-02-05 17:07:14 +0800

    mod - Mon, Feb  5, 2024 17:07:14

--- src/test/java/com/packt/cardatabase/CardatabaseApplicationTests.java
+++ src/test/java/com/packt/cardatabase/CardatabaseApplicationTests.java
@@ -1,13 +1,30 @@
 package com.packt.cardatabase;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import javax.swing.Spring;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.packt.cardatabase.web.CarController;
 
 @SpringBootTest
 class CardatabaseApplicationTests {
+	/*
+	 * We use field injection here, which is well-suited for test classes because
+	 * you will never instantiate your test classes directly. You can read more
+	 * about dependency injection of test fixtures in the Spring documentation:
+	 * https://docs.spring.io/spring-framework/reference/testing/testcontext-
+	 * framework/fixture-di.html.
+	 */
+	@Autowired
+	private CarController carController;
 
 	@Test
 	void contextLoads() {
+		assertThat(carController).isNotNull();
 	}
 
 }
