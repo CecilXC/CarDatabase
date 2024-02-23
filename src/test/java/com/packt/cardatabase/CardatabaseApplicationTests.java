907fd971691f17dde7ce85dbfa02b399c89b8ea7
XC <xc@abc.com>
2024-02-23 16:31:40 +0800

    mod - Fri, Feb 23, 2024 16:31:40

--- src/test/java/com/packt/cardatabase/CardatabaseApplicationTests.java
+++ src/test/java/com/packt/cardatabase/CardatabaseApplicationTests.java
@@ -15,8 +15,7 @@ class CardatabaseApplicationTests {
 	 * We use field injection here, which is well-suited for test classes because
 	 * you will never instantiate your test classes directly. You can read more
 	 * about dependency injection of test fixtures in the Spring documentation:
-	 * https://docs.spring.io/spring-framework/reference/testing/testcontext-
-	 * framework/fixture-di.html.
+	 * https://docs.spring.io/spring-framework/reference/testing/testcontext-framework/fixture-di.html.
 	 */
 	@Autowired
 	private CarController carController;
