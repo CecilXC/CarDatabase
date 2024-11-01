58937e68a1991fcb05e6a774760e1267a8c8b959
XC <xc@abc.com>
2024-11-01 14:22:01 +0800

    mod - Fri Nov  1 14:22:01 CST 2024

--- src/main/java/com/packt/cardatabase/AuthEntryPoint.java
+++ src/main/java/com/packt/cardatabase/AuthEntryPoint.java
@@ -18,10 +18,10 @@ public class AuthEntryPoint implements AuthenticationEntryPoint {
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response,
             AuthenticationException authException) throws IOException, ServletException {
-        /* response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
         response.setContentType(MediaType.APPLICATION_JSON_VALUE);
         PrintWriter writer = response.getWriter();
-        writer.println("Error: " + authException.getMessage()); */
+        writer.println("Error: " + authException.getMessage());
     }
 
 }
